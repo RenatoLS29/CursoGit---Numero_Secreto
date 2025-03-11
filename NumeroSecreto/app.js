719a8c9904beb6115508f3833c517b5fea0e9f17
RenatoLS29 <renatosouzaleite816@gmail.com>
2025-03-11 12:35:24 -0300

    alterar

--- NumeroSecreto/app.js
+++ NumeroSecreto/app.js
@@ -13,8 +13,7 @@ function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
-
-exibirMensagemInicial();
+exibirMensagemIncial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
